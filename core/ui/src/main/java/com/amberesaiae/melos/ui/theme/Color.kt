178dac0aefdccbac979e05f57f6b5edfb6faf3e9
Amberesaiae <isaiahamber5@gmail.com>
2026-03-23 12:16:59 +0000

    feat: Add Color.kt with Melos Material 3 color palette

---
+++ core/ui/src/main/java/com/amberesaiae/melos/ui/theme/Color.kt
@@ -0,0 +1,128 @@
+package com.amberesaiae.melos.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Primary Colors - Blue/Indigo Theme
+val Blue500 = Color(0xFF3F51B5)
+val Blue700 = Color(0xFF303F9F)
+val Blue300 = Color(0xFF7986CB)
+val Blue100 = Color(0xFFC5CAE9)
+val OnBlue = Color(0xFFFFFFFF)
+
+val Indigo500 = Color(0xFF3F51B5)
+val Indigo400 = Color(0xFF5C6BC0)
+val Indigo100 = Color(0xFFE8EAF6)
+val OnIndigo = Color(0xFFFFFFFF)
+
+// Secondary Colors - Teal/Cyan Complementary
+val Teal500 = Color(0xFF009688)
+val Teal200 = Color(0xFF80CBC4)
+val Teal100 = Color(0xFFB2DFDB)
+val OnTeal = Color(0xFF003D36)
+
+val Cyan500 = Color(0xFF00BCD4)
+val Cyan200 = Color(0xFF80DEEA)
+val Cyan100 = Color(0xFFB2EBF2)
+val OnCyan = Color(0xFF004D52)
+
+// Tertiary/Accent Colors
+val Amber500 = Color(0xFFFFC107)
+val Amber200 = Color(0xFFFFECB3)
+val OnAmber = Color(0xFF4A3B00)
+
+val Pink500 = Color(0xFFE91E63)
+val Pink200 = Color(0xFFF48FB1)
+val OnPink = Color(0xFF5C0027)
+
+// Neutral Colors
+val Gray50 = Color(0xFFFAFAFA)
+val Gray100 = Color(0xFFF5F5F5)
+val Gray200 = Color(0xFFEEEEEE)
+val Gray300 = Color(0xFFE0E0E0)
+val Gray400 = Color(0xFFBDBDBD)
+val Gray500 = Color(0xFF9E9E9E)
+val Gray600 = Color(0xFF757575)
+val Gray700 = Color(0xFF616161)
+val Gray800 = Color(0xFF424242)
+val Gray900 = Color(0xFF212121)
+
+val White = Color(0xFFFFFFFF)
+val Black = Color(0xFF000000)
+
+// Error Colors
+val Red500 = Color(0xFFF44336)
+val Red400 = Color(0xFFEF5350)
+val Red100 = Color(0xFFFFCDD2)
+val OnRed = Color(0xFF690005)
+
+// Light Theme Color Scheme
+val LightPrimary = Indigo500
+val LightOnPrimary = OnIndigo
+val LightPrimaryContainer = Indigo100
+val LightOnPrimaryContainer = Indigo500
+
+val LightSecondary = Teal500
+val LightOnSecondary = OnTeal
+val LightSecondaryContainer = Teal100
+val LightOnSecondaryContainer = Teal500
+
+val LightTertiary = Cyan500
+val LightOnTertiary = OnCyan
+val LightTertiaryContainer = Cyan100
+val LightOnTertiaryContainer = Cyan500
+
+val LightError = Red500
+val LightOnError = OnRed
+val LightErrorContainer = Red100
+val LightOnErrorContainer = Red500
+
+val LightBackground = Gray50
+val LightOnBackground = Gray900
+val LightSurface = White
+val LightOnSurface = Gray900
+val LightSurfaceVariant = Gray200
+val LightOnSurfaceVariant = Gray600
+
+val LightOutline = Gray400
+val LightOutlineVariant = Gray300
+val LightInverseSurface = Gray800
+val LightInverseOnSurface = Gray100
+val LightInversePrimary = Indigo300
+val LightShadow = Black.copy(alpha = 0.1f)
+val LightSurfaceTint = Indigo500
+
+// Dark Theme Color Scheme
+val DarkPrimary = Indigo300
+val DarkOnPrimary = Indigo700
+val DarkPrimaryContainer = Indigo600
+val DarkOnPrimaryContainer = Indigo100
+
+val DarkSecondary = Teal200
+val DarkOnSecondary = Teal700
+val DarkSecondaryContainer = Teal600
+val DarkOnSecondaryContainer = Teal100
+
+val DarkTertiary = Cyan200
+val DarkOnTertiary = Cyan700
+val DarkTertiaryContainer = Cyan600
+val DarkOnTertiaryContainer = Cyan100
+
+val DarkError = Red400
+val DarkOnError = OnRed
+val DarkErrorContainer = Red900
+val DarkOnErrorContainer = Red100
+
+val DarkBackground = Gray900
+val DarkOnBackground = Gray50
+val DarkSurface = Gray800
+val DarkOnSurface = Gray50
+val DarkSurfaceVariant = Gray700
+val DarkOnSurfaceVariant = Gray300
+
+val DarkOutline = Gray600
+val DarkOutlineVariant = Gray700
+val DarkInverseSurface = Gray100
+val DarkInverseOnSurface = Gray800
+val DarkInversePrimary = Indigo500
+val DarkShadow = Black.copy(alpha = 0.3f)
+val DarkSurfaceTint = Indigo300
